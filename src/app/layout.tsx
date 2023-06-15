6e6c5fbed65715bc2e3f89e93528865a7c61a41e
Jordan Braz <jordanbraz@outlook.com>
2023-06-15 17:22:57 -0300

    update files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
-import { Navbar } from './components/Navbar'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-principal' })
 const baiJamjuree = BaiJamjuree({
